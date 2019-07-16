793bd712556356d636e2b638d126e684f43801c6
kouts <koutsaftakis@gmail.com>
2019-07-16 18:58:50 +0300

    Resolved css as an empty module, fixed css already loaded check

--- dist/css.js
+++ dist/css.js
@@ -5,13 +5,13 @@
   var originalInstantiate = systemPrototype.instantiate;
 
   systemPrototype.instantiate = function () {
-    var loader = this;
     var url = arguments[0];
 
     if (url.slice(-4) === '.css') {
       return new Promise(function (resolve, reject) {
         if (document.querySelector('link[href="' + url + '"]') || document.querySelector('link[href="' + url.replace(location.protocol + '//' + location.hostname, '') + '"]')) {
-          reject(Error('Style ' + url + ' has already been loaded'));
+          reject(Error('Style ' + url + ' has already been loaded using another way.'));
+          return;
         }
 
         var link = document.createElement('link');
@@ -21,17 +21,19 @@
         document.head.appendChild(link);
 
         link.onload = function () {
-          resolve(loader.getRegister());
+          resolve([[], function () {
+            return {};
+          }]);
         };
 
-        link.onerror = function () {
+        link.onerror = function (e) {
           var href = document.querySelector('link[href="' + url + '"]');
 
           if (href) {
             href.parentElement.removeChild(href);
           }
 
-          reject(Error('Style ' + url + ' has failed loading'));
+          reject(e);
         };
       });
     } else {
--- src/css.js
+++ src/css.js
@@ -5,7 +5,7 @@
   const originalInstantiate = systemPrototype.instantiate;
 
   systemPrototype.instantiate = function () {
-    const loader = this;
+    // const loader = this;
     const url = arguments[0];
     if (url.slice(-4) === '.css'){
       return new Promise(function(resolve, reject){
@@ -13,7 +13,8 @@
           document.querySelector('link[href="'+url+'"]') || 
           document.querySelector('link[href="'+url.replace(location.protocol+'//'+location.hostname, '')+'"]')
         ){
-          reject(Error('Style '+url+' has already been loaded'));
+          reject(Error('Style '+url+' has already been loaded using another way.'));
+          return;
         }
         let link = document.createElement('link');
         link.type = 'text/css';
@@ -22,14 +23,16 @@
         document.head.appendChild(link);
         link.onload = function(){
           // console.log('%c Style '+url+' has been loaded', 'color: green');
-          resolve(loader.getRegister());
+          resolve([[], function (){
+            return {};
+          }]);
         };
-        link.onerror = function() {
+        link.onerror = function(e) {
           let href = document.querySelector('link[href="'+url+'"]');
           if(href){
             href.parentElement.removeChild(href);
           }
-          reject(Error('Style '+url+' has failed loading'));
+          reject(e);
         };
       });
     } else {
